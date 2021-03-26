4a27d7321339136c7bb6af734c5e613f3aa4455a
Kasey <kaseykaufmann@yahoo.com>
2021-03-26 00:26:19 -0400

    update: created static design of prof, skills, saving_throws

--- client/app/components/DashBoard/index.js
+++ client/app/components/DashBoard/index.js
@@ -1,31 +1,311 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import Header from '../shared/Header';
 
+import './styles.scss';
+
+const TEMP_DATA = {
+  name: 'Liir Thropp',
+  level: 2,
+  experience: {
+    current: 647,
+    threshhold: 960,
+  },
+  race: {
+    name: 'Rogue',
+    description: 'BLAH BLAH BLAH',
+  },
+  class: [
+    {
+      name: 'Halfling',
+      levels: 2,
+    },
+  ],
+  misc_proficiencies: {
+    armor: [{ name: 'Light Armor' }],
+    weapons: [
+      { name: 'Crossbows' },
+      { name: 'Hand' },
+      { name: 'Longswords' },
+      { name: 'Rapiers' },
+      { name: 'Shortswords' },
+      { name: 'Simple Weapons' },
+    ],
+    tools: [{ name: "Thieve's Tools" }],
+    languages: [{ name: 'Common' }, { name: 'Halfling' }],
+  },
+  ability_scores: {
+    str: {
+      score: 9,
+      modifier: 2,
+      advantage: -1,
+    },
+    dex: {
+      score: 16,
+      modifier: 4,
+      advantage: 3,
+    },
+    con: {
+      score: 1,
+      modifier: 1,
+      advantage: 1,
+    },
+    int: {
+      score: 5,
+      modifier: -1,
+      advantage: -3,
+    },
+    wis: {
+      score: 11,
+      modifier: 3,
+      advantage: 0,
+    },
+    cha: {
+      score: 14,
+      modifier: 3,
+      advantage: 2,
+    },
+  },
+  saving_throws: {
+    Strength: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: -1,
+    },
+    Dexterity: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: 5,
+    },
+    Constitution: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: 1,
+    },
+    Intelligence: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: -1,
+    },
+    Wisdom: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: 0,
+    },
+    Charisma: {
+      proficiency: 1,
+      modifier: 0,
+      advantage: 2,
+    },
+  },
+  skills: {
+    // should these skills have an associated ability, or are we getting that from the api?
+    acrobatics: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 5,
+    },
+    animal_handling: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 0,
+    },
+    arcana: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -3,
+    },
+    athletics: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -1,
+    },
+    deception: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 4,
+    },
+    history: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -3,
+    },
+    insight: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 0,
+    },
+    intimidation: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 4,
+    },
+    investigation: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -3,
+    },
+    medicine: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 0,
+    },
+    nature: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -3,
+    },
+    perception: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 2,
+    },
+    performance: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 2,
+    },
+    persuasion: {
+      proficiency: false,
+      modifier: 0,
+      advantage: -4,
+    },
+    religion: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 3,
+    },
+    sleight_of_hand: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 5,
+    },
+    stealth: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 5,
+    },
+    survival: {
+      proficiency: false,
+      modifier: 0,
+      advantage: 0,
+    },
+  },
+};
 
 export const DashBoard = () => {
-    const user = useSelector(state => state.user);
-    console.log(user);
+  const {
+    name,
+    level,
+    experience,
+    race,
+    misc_proficiencies,
+    ability_scores,
+    saving_throws,
+    skills,
+  } = TEMP_DATA;
+  const user = useSelector(state => state.user);
+  console.log(user);
 
-    const character = useSelector(state => state.characters);
-    console.log(character);
+  const character = useSelector(state => state.characters);
+  console.log(character);
 
   return (
-    <div className="container">
-      <h1 className="display-1 text-center text-light title">
-        Dashboard
-        <Link to="/logout">
-          <button
-            type="button"
-            className="btn btn-outline-danger log-out-button"
-          >
-            Log Out
-          </button>
-        </Link>
-      </h1>
-      {Object.keys(character).map((key) => {
-            return (<p>{character[key].race?.index + " " + character[key].class?.index}</p>)
+    <div className="dashboard">
+      <Header />
+      <div className="container">
+        <h1 className="display-1 text-center text-light title">
+          Dashboard
+          <Link to="/logout">
+            <button
+              type="button"
+              className="btn btn-outline-danger log-out-button"
+            >
+              Log Out
+            </button>
+          </Link>
+        </h1>
+        {Object.keys(character).map(key => {
+          return (
+            <p>
+              {character[key].race?.index + ' ' + character[key].class?.index}
+            </p>
+          );
         })}
+        <SavingThrowsCard saving_throws={saving_throws} />
+        <SkillsCard skills={skills} />
+        <ProficieniesCard misc_proficiencies={misc_proficiencies} />
+      </div>
+    </div>
+  );
+};
+
+const SavingThrowsCard = ({ saving_throws }) => {
+  return (
+    <div className="card translucent-card">
+      <h5 className="card-title">Saving Throws</h5>
+      <div className="card content-card description-card">
+        <ul>
+          {Object.entries(saving_throws).map(saving_throw => {
+            return (
+              <li key={saving_throw[0]}>
+                <p>
+                  {saving_throw[1].advantage >= 0 && '+'}
+                  {saving_throw[1].advantage} {saving_throw[0]}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const SkillsCard = ({ skills }) => {
+  return (
+    <div className="card translucent-card">
+      <h5 className="card-title">Skills</h5>
+      <div className="card content-card description-card">
+        <ul>
+          {Object.entries(skills).map(skill => {
+            return (
+              <li key={skill[0]}>
+                <p>
+                  {skill[1].advantage >= 0 && '+'}
+                  {skill[1].advantage} {skill[0]}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ProficieniesCard = ({ misc_proficiencies }) => {
+  console.log(misc_proficiencies);
+  return (
+    <div className="card translucent-card">
+      <h5 className="card-title">Proficiencies</h5>
+      <div className="card content-card description-card">
+        {Object.entries(misc_proficiencies).map(misc_proficiency => {
+          return (
+            <p key={misc_proficiency[0]}>
+              <div className="text-uppercase w-auto">{misc_proficiency[0]}</div>
+              -{' '}
+              {misc_proficiency[1].map((items, idx) => (
+                <div key={idx}>
+                  {items.name}
+                  {idx !== 0 && idx < misc_proficiency[1].length && ', '}
+                </div>
+              ))}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,181 +1,201 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require("../models/User.js");
-const config = require("../../config/config");
+const User = require('../models/User.js');
+const config = require('../../config/config');
 
 // Projections  https://docs.mongodb.com/manual/tutorial/project-fields-from-query-results/
 const projections = {
-    public: { _id: 0, password: 0 },
-    private: { _id: 0, password: 0 },
-    friends: { _id: 0, password: 0 }
-}
+  public: { _id: 0, password: 0 },
+  private: { _id: 0, password: 0 },
+  friends: { _id: 0, password: 0 },
+};
 
 // Middleware that attaches character info to the req
 const attachUserInfo = (req, res, next) => {
-    User.findById(req.locals.auth.id, projections.all).then(user => {
-        if (!user) return throwError(res, 401, "No Content.");
-        req.user = user;
-        return next();
-    }).catch(err => throwError(res, 401, "No Content.", err));
-}
+  User.findById(req.locals.auth.id, projections.all)
+    .then(user => {
+      if (!user) return throwError(res, 401, 'No Content.');
+      req.user = user;
+      return next();
+    })
+    .catch(err => throwError(res, 401, 'No Content.', err));
+};
 
 // Identical error handling per route for security reasons
 const throwError = (res, code, message, err) => {
-    if (config.isDev){
-        if (err) console.log("Unexpected error: ", err);
-    }
-    return res.status(code).json({message: message});
-}
+  if (config.isDev) {
+    if (err) console.log('Unexpected error: ', err);
+  }
+  return res.status(code).json({ message: message });
+};
 
 // Display lists of users.
 const user_list = (req, res) => {
-    const query = req.query;
-    const projection = projections.public
-    if(query){
-        User.find(query, projection).then(users => {
-            if(!users) throwError(res, 500, "Database Error");
-            return res.status(200).json(users);
-        }).catch(err => {
-            return throwError(res, 500, "Database Error", err);
-        })
-    } else {
-        return res.status(501).send('NOT IMPLEMENTED: user list');
-    }
-}
+  const query = req.query;
+  const projection = projections.public;
+  if (query) {
+    User.find(query, projection)
+      .then(users => {
+        if (!users) throwError(res, 500, 'Database Error');
+        return res.status(200).json(users);
+      })
+      .catch(err => {
+        return throwError(res, 500, 'Database Error', err);
+      });
+  } else {
+    return res.status(501).send('NOT IMPLEMENTED: user list');
+  }
+};
 
 // Display detail page for a specific user.
 const user_detail = (req, res) => {
-    // TODO: Alter this check if we want user info to be truly public, i.e. show the existance of users to the outside world
-    if (!res.locals.auth) return throwError(res, 401, "No Content.");
+  // TODO: Alter this check if we want user info to be truly public, i.e. show the existance of users to the outside world
+  if (!res.locals.auth) return throwError(res, 401, 'No Content.');
 
-    let projection = projections.public;
+  let projection = projections.public;
 
-    if (res.locals.auth.username == req.params.userid) {
-        projection = projections.private
-    } else if (res.locals.auth.username){
-        // Friend/party exclusive info?
-        User.find({username: res.locals.auth.username}).then(user => {
-            if (user && user.friends && user.friends.includes(res.locals.auth.id)) {
-                projection = projections.friends
-            }
-        })
-    }
+  if (res.locals.auth.username == req.params.userid) {
+    projection = projections.private;
+  } else if (res.locals.auth.username) {
+    // Friend/party exclusive info?
+    User.find({ username: res.locals.auth.username }).then(user => {
+      if (user && user.friends && user.friends.includes(res.locals.auth.id)) {
+        projection = projections.friends;
+      }
+    });
+  }
 
-    // Defaults to username parameter
-    User.find({username: req.params.username}, projection).then(user => {
-        if (!user) return throwError(res, 401, "No Content.");
-        return res.status(200).json(user);
-    }).catch(err => throwError(res, 401, "No Content.", err));
-}
+  // Defaults to username parameter
+  User.find({ username: req.params.username }, projection)
+    .then(user => {
+      if (!user) return throwError(res, 401, 'No Content.');
+      return res.status(200).json(user);
+    })
+    .catch(err => throwError(res, 401, 'No Content.', err));
+};
 
 // Delete user
 const delete_user = (req, res) => {
-    let query = { username: req.params.username }
+  let query = { username: req.params.username };
 
-    //Checks that the user id's match
-    User.findOne(query).then(user => {
-        if(!user) return throwError(res, 403, "Action Forbidden.");
-        if(user.id !== res.locals.auth.id) return throwError(res, 403, "Action Forbidden.");
-    })
+  //Checks that the user id's match
+  User.findOne(query).then(user => {
+    if (!user) return throwError(res, 403, 'Action Forbidden.');
+    if (user.id !== res.locals.auth.id)
+      return throwError(res, 403, 'Action Forbidden.');
+  });
 
-    User.findOneAndDelete(query, (err, deletedUser) => {
-        if (err) return throwError(res, 403, "Action Forbidden.", err);
-        if(deletedUser){
-            return res.status(200).send('Successfully deleted user.');
-        } else {
-            return throwError(res, 403, "Action Forbidden.");
-        }
-    })
-}
+  User.findOneAndDelete(query, (err, deletedUser) => {
+    if (err) return throwError(res, 403, 'Action Forbidden.', err);
+    if (deletedUser) {
+      return res.status(200).send('Successfully deleted user.');
+    } else {
+      return throwError(res, 403, 'Action Forbidden.');
+    }
+  });
+};
 
 // Update user
-const update_user = async(req, res) => {
-    let query = { username: req.params.username }
-    let oldUser = {}
-    console.log("Attempting to update user:", req.params.username);
-    //Checks that the user id's match
-    User.findOne(query).then(user => {
-        if(!user) return throwError(res, 403, "Action forbidden.");
-        if(user.id !== res.locals.auth.id) return throwError(res, 403, "Action forbidden.")
-        oldUser = user;
-        console.log("Found user:", req.params.username);
-    }).catch(err => throwError(res, 403, "Action forbidden.", err))
+const update_user = async (req, res) => {
+  let query = { username: req.params.username };
+  let oldUser = {};
+  console.log('Attempting to update user:', req.params.username);
+  //Checks that the user id's match
+  User.findOne(query)
+    .then(user => {
+      if (!user) return throwError(res, 403, 'Action forbidden.');
+      if (user.id !== res.locals.auth.id)
+        return throwError(res, 403, 'Action forbidden.');
+      oldUser = user;
+      console.log('Found user:', req.params.username);
+    })
+    .catch(err => throwError(res, 403, 'Action forbidden.', err));
 
-    const updates = req.body
+  const updates = req.body;
 
-    // TODO: Add old password check to change password
-    if (updates.password) {
-        console.log("Updating password.");
-        try {
-            const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-            updates.password = hashedPassword;
-        } catch(err) {
-            if(err) return throwError(res, 403, "Action forbidden.", err);
-        }
+  // TODO: Add old password check to change password
+  if (updates.password) {
+    console.log('Updating password.');
+    try {
+      const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+      updates.password = hashedPassword;
+    } catch (err) {
+      if (err) return throwError(res, 403, 'Action forbidden.', err);
     }
+  }
 
-    console.log("Applying:", updates);
+  console.log('Applying:', updates);
 
-    User.findOneAndUpdate(query, {$set: updates}, { new: true })
+  User.findOneAndUpdate(query, { $set: updates }, { new: true })
     .then(newUser => {
-        if(!newUser) 
-            return throwError(res, 403, "Action forbidden.")
-        
-        newUser.save()
-        console.log("Successfully updated user:", req.params.username);
-        
-        if(Object.keys(updates).includes('password')){
-            return res.clearCookie('auth').status(200).json({old: oldUser, new: newUser});
-        } else {
-            return res.status(200).json({old: oldUser, new: newUser});
-        }
-    }).catch(err => throwError(res, 403, "Action forbidden.", err));
-    // return res.status(500).send('hashing error');
-}
+      if (!newUser) return throwError(res, 403, 'Action forbidden.');
+
+      newUser.save();
+      console.log('Successfully updated user:', req.params.username);
+
+      if (Object.keys(updates).includes('password')) {
+        return res
+          .clearCookie('auth')
+          .status(200)
+          .json({ old: oldUser, new: newUser });
+      } else {
+        return res.status(200).json({ old: oldUser, new: newUser });
+      }
+    })
+    .catch(err => throwError(res, 403, 'Action forbidden.', err));
+  // return res.status(500).send('hashing error');
+};
 
 // Register user
 const register_user = (req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err || !hash) return throwError(res, 500, "Error registering user.");
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
-        });
-        //TODO: Write this function
-        // sendAuthenticationEmail(req, user);
-        User.create(user).then(newUser => {
-            if(!newUser) return throwError(res, 500, "Error registering user.")
-            return res.status(200).json(newUser);
-        }).catch(err => throwError(res, 500, "Error registering user.", err));
-    })
-}
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err || !hash) return throwError(res, 500, 'Error registering user.');
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+    //TODO: Write this function
+    // sendAuthenticationEmail(req, user);
+    User.create(user)
+      .then(newUser => {
+        if (!newUser) return throwError(res, 500, 'Error registering user.');
+        return res.status(200).json(newUser);
+      })
+      .catch(err => throwError(res, 500, 'Error registering user.', err));
+  });
+};
 
 // Login user
 const login_user = (req, res) => {
-    // Log in with either username or email
-    let query = {}
-    if (req.body.email && !req.body.username){
-        query = { email: req.body.email };
-    } else if (req.body.username && !req.body.email){
-        query = { username: req.body.username };
-    }
+  // Log in with either username or email
+  let query = {};
+  if (req.body.email && !req.body.username) {
+    query = { email: req.body.email };
+  } else if (req.body.username && !req.body.email) {
+    query = { username: req.body.username };
+  }
 
-    User.findOne(query).then(user => {
-        console.log("Attempting to sign in user:", user.username);
-        if (!user) return throwError(res, 401, "Invalid credentials.");
-        bcrypt.compare(req.body.password, user.password).then(response => {
-            if (response === false) return throwError(res, 401, "Invalid credentials.");
-            // TODO: Research if this is safe to leave id in payload
-            const jwtPayload = { 
-                id: user.id,
-                username: user.username,
-            };
-            console.log("Successfully signed in user: ", user.username);
-            jwt.sign(jwtPayload, config.jwt.secret, (error, token) => {
-                if (error) return throwError(res, 401, "Invalid credentials.", error)
-                /*
+  User.findOne(query)
+    .then(user => {
+      console.log('Attempting to sign in user:', user.username);
+      if (!user) return throwError(res, 401, 'Invalid credentials.');
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then(response => {
+          if (response === false)
+            return throwError(res, 401, 'Invalid credentials.');
+          // TODO: Research if this is safe to leave id in payload
+          const jwtPayload = {
+            id: user.id,
+            username: user.username,
+          };
+          console.log('Successfully signed in user: ', user.username);
+          jwt.sign(jwtPayload, config.jwt.secret, (error, token) => {
+            if (error)
+              return throwError(res, 401, 'Invalid credentials.', error);
+            /*
                 //MAY REQUIRE cors package with use with fetch()...CHECK!
                 //TODO: EMAIL VERIFICATION
                 if (user.email_verified === false) {
@@ -184,15 +204,32 @@ const login_user = (req, res) => {
                     return done(error);
                 }
                 */
-                return res.status(200).cookie('auth', token, config.jwt.options).json(user);
-            });
-        }).catch(err => throwError(res, 401, "Invalid credentials.", err));
-    }).catch(err => throwError(res, 401, "Invalid credentials.", err));
-}
+            return res
+              .status(200)
+              .cookie('auth', token, config.jwt.options)
+              .json(user);
+          });
+        })
+        .catch(err => throwError(res, 401, 'Invalid credentials.', err));
+    })
+    .catch(err => throwError(res, 401, 'Invalid credentials.', err));
+};
 
 // Logout user
 const logout_user = (req, res) => {
-    res.clearCookie('auth').status(200).send("Successfully logged out. Cookie cleared.");
-}
+  res
+    .clearCookie('auth')
+    .status(200)
+    .send('Successfully logged out. Cookie cleared.');
+};
 
-module.exports = { user_list, user_detail, delete_user, update_user, register_user, login_user, logout_user, attachUserInfo }
+module.exports = {
+  user_list,
+  user_detail,
+  delete_user,
+  update_user,
+  register_user,
+  login_user,
+  logout_user,
+  attachUserInfo,
+};
