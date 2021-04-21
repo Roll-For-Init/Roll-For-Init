eb518ef72255e9f4e3ccba0e29034ad21e241d2a
Spencer Bass <spencerbass1001@gmail.com>
2021-04-21 09:55:16 -0400

    Implement axios/redux cache Changed import from axios npm module to our project axios module in all files that called axios.get

--- client/app/redux/services/character.service.js
+++ client/app/redux/services/character.service.js
@@ -1,15 +1,27 @@
-const axios = require('axios');
+const axios = require('../utils/axios');
 const apiCaller = require('../../utils/apiCaller');
-const {parseEquipment, fillModel} = require('../../utils/characterValidator');
+const { parseEquipment, fillModel } = require('../../utils/characterValidator');
 
 const API_URL = '/api/';
 
 const validateCharacter = async characterData => {
-    let equipment = await parseEquipment(characterData.equipment, [...characterData.class.proficiencies.Weapons||[], ...characterData.race.proficiencies.Weapons||[], ...characterData.background.proficiencies.Weapons||[]], [...characterData.class.proficiencies.Armor||[], ...characterData.race.proficiencies.Armor||[], ...characterData.background.proficiencies.Armor||[]]);
-    return await fillModel(equipment, characterData).then((validatedCharacter) => {
-        return validatedCharacter
-    });    
-}
+  let equipment = await parseEquipment(
+    characterData.equipment,
+    [
+      ...(characterData.class.proficiencies.Weapons || []),
+      ...(characterData.race.proficiencies.Weapons || []),
+      ...(characterData.background.proficiencies.Weapons || []),
+    ],
+    [
+      ...(characterData.class.proficiencies.Armor || []),
+      ...(characterData.race.proficiencies.Armor || []),
+      ...(characterData.background.proficiencies.Armor || []),
+    ]
+  );
+  return await fillModel(equipment, characterData).then(validatedCharacter => {
+    return validatedCharacter;
+  });
+};
 const createCharacter = characterData => {
   return axios.post(API_URL + 'characters/create', characterData);
 };
@@ -24,34 +36,36 @@ const deleteCharacter = characterData => {
   return axios.delete(API_URL + 'characters/' + id + '/', characterData);
 };
 
-const getIndexedList = (type) => {
-    return axios.get(`${API_URL}${type}`).then((items) => {
-        return items.data.results;
-    });
+const getIndexedList = type => {
+  return axios.get(`${API_URL}${type}`).then(items => {
+    return items.data.results;
+  });
 };
 
-const getSubList = (url) => {
-    return axios.get(`${API_URL}${url}`).then((items) => {
-        return items.data;
-    })
+const getSubList = url => {
+  return axios.get(`${API_URL}${url}`).then(items => {
+    return items.data;
+  });
 };
 
-const getRaceInfo = (race) => {
-    return apiCaller.propogateRacePointer(race);
+const getRaceInfo = race => {
+  return apiCaller.propogateRacePointer(race);
 };
-const getRaceDetails = (race) => {
-    return apiCaller.getRaceMiscDescriptions(race);
-}
-
-const getClassInfo = (theClass) => {
-    return apiCaller.classCaller(theClass);
+const getRaceDetails = race => {
+  return apiCaller.getRaceMiscDescriptions(race);
 };
 
-const getClassDetails = (theClass) => {
-    return apiCaller.getClassDescriptions(theClass);
-}
+const getClassInfo = theClass => {
+  return apiCaller.classCaller(theClass);
+};
+
+const getClassDetails = theClass => {
+  return apiCaller.getClassDescriptions(theClass);
+};
 const getBackgroundInfo = background => {
-  return apiCaller.backgroundCaller(`${API_URL}backgrounds/${background.index}`);
+  return apiCaller.backgroundCaller(
+    `${API_URL}backgrounds/${background.index}`
+  );
 };
 
 const getAbilityScoreInfo = abilityScore => {
@@ -63,26 +77,31 @@ const getEquipmentDetails = equipment => {
   return apiCaller.equipmentDetails(equipment);
 };
 const getSpells = (theClass, levels) => {
-    let url = `${API_URL}classes/${theClass.index.toLowerCase()}/levels/`;
-    let subclassSpells = theClass.subclass && theClass.subclass_spells ? theClass.subclass.subclass_spells : [];
-    return apiCaller.getSpellCards(levels, url, subclassSpells);
+  let url = `${API_URL}classes/${theClass.index.toLowerCase()}/levels/`;
+  let subclassSpells =
+    theClass.subclass && theClass.subclass_spells
+      ? theClass.subclass.subclass_spells
+      : [];
+  return apiCaller.getSpellCards(levels, url, subclassSpells);
 };
 const getLevel = (theClass, level) => {
-    return axios.get(API_URL + 'classes/' + theClass + 'levels/' + level);
+  return axios.get(API_URL + 'classes/' + theClass + 'levels/' + level);
 };
 
-export default { validateCharacter,
-    createCharacter,
-    updateCharacter,
-    deleteCharacter,
-    getIndexedList,
-    getSubList,
-    getRaceInfo,
-    getClassInfo,
-    getBackgroundInfo,
-    getAbilityScoreInfo,
-    getRaceDetails,
-    getClassDetails,
-    getEquipmentDetails,
-    getSpells,
-    getLevel }
+export default {
+  validateCharacter,
+  createCharacter,
+  updateCharacter,
+  deleteCharacter,
+  getIndexedList,
+  getSubList,
+  getRaceInfo,
+  getClassInfo,
+  getBackgroundInfo,
+  getAbilityScoreInfo,
+  getRaceDetails,
+  getClassDetails,
+  getEquipmentDetails,
+  getSpells,
+  getLevel,
+};
--- client/app/utils/characterValidator.js
+++ client/app/utils/characterValidator.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+const axios = require('./axios')
 
 const parseEquipment = (items, weaponProficiencies, armorProficiencies) => {
     const equipment = {
